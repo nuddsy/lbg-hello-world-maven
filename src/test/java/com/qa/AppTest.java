4c3fd0c8dcd8583f0f4f24fbe3d2cfa9bbcd2a57
Matt Nudds <matthewnudds1@gmail.com>
2024-11-04 14:55:47 +0000

    Added test, expected to fail

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -43,4 +43,17 @@ public class AppTest
         // Assert
         assertEquals( expectedResponse, actualResponse);
     }
+
+    @Test
+    public void test_sayGoodMorning_should_return_Good_Morning() {
+        //Arrange
+        String actualResponse = "";
+        String expectedResponse = "Good Morning"
+
+        //Act
+        actualResponse = sayGoodMorning();
+
+        //Assert
+        assertEquals( expectedResponse, actualResponse );
+    }
 }
